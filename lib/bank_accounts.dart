3b7461db4b869ad43d0caaa7fcc3820a034829d3
konul <konul.eminova39@gmail.com>
2019-09-11 18:11:18 +0400

    push notification done

--- lib/bank_accounts.dart
+++ lib/bank_accounts.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:zeta_bank/item/bank_account_item.dart';
 import 'package:zeta_bank/model/bank_accounts.dart';
@@ -15,7 +16,68 @@ class BankAccountsPage extends StatefulWidget {
 class BankAccoquntsState extends State<BankAccountsPage> {
   String accessToken;
   int userId;
-
+  int counter = 2;
+  String _homeScreenText = "Waiting for token...";
+  String _messageText = "Waiting for message...";
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  @override
+  void initState() {
+    super.initState();
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        setState(() {
+          _messageText = "Push Messaging message: $message";
+        });
+        print("onMessage: $message");
+        showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              content: ListTile(
+                title: Text(message['notification']['title']),
+                subtitle: Text(message['notification']['body']),
+              ),
+              actions: <Widget>[
+                FlatButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('Close')),
+                RaisedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Confirm',style: TextStyle(color: Colors.white),),
+                )
+              ],
+            ));
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        setState(() {
+          _messageText = "Push Messaging message: $message";
+        });
+        print("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) async {
+        setState(() {
+          _messageText = "Push Messaging message: $message";
+        });
+        print("onResume: $message");
+      },
+    );
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(sound: true, badge: true, alert: true));
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {
+      print("Settings registered: $settings");
+    });
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      setState(() {
+        _homeScreenText = "Push Messaging token: $token";
+      });
+      print(_homeScreenText);
+    });
+  }
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -76,10 +138,4 @@ class BankAccoquntsState extends State<BankAccountsPage> {
   }
 
   getAsunc() async {}
-
-  @override
-  void initState() {
-    super.initState();
-
-  }
 }
